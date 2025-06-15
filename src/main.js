07aa84f5c2e10b8b1b91aeef6960f34e830016c2
Yusnegg <yusnelleyvag@gmail.com>
2025-06-15 19:09:30 -0400

    ⚡: Fix main entry point in package.json and implement clipboard reading functionality in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ const { app, BrowserWindow, Tray, Menu, clipboard } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
+text_latest = ""
+
+function readClipboard(){
+    setInterval(() => {
+        text_latest = clipboard.readText()
+        printClipboard(text_latest)
+    }, 2000);
+}
+
+function printClipboard(text){
+    console.log(text)
+}
+
+
 app.whenReady().then(() => {
     const contextMenu = Menu.buildFromTemplate([
         { label: 'Salir', click: () => app.quit() }
@@ -16,4 +30,5 @@ app.whenReady().then(() => {
     });
     win.loadFile('index.html');
 
+    readClipboard();
 });
